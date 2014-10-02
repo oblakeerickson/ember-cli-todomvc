b55cecabd926d8b45de635aa8ab621f9ef37192e
Blake Erickson <o.blakeerickson@gmail.com>
2014-10-02 08:15:25 -0600

    finished rest of todo features

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   actions: {
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
+    },
     createTodo: function() {
       // Get the todo title set by the "New Todo" text field
       var title = this.get('newTitle');
@@ -20,9 +25,26 @@ export default Ember.ArrayController.extend({
       todo.save();
     }
   },
+  hasCompleted: function() {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
 
   remaining: function() {
     return this.filterBy('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  allAreDone: function(key, value) {
+    if (value === undefined) {
+      return !!this.get('length') && this.everyProperty('isCompleted', true);
+    } else {
+      this.setEach('isCompleted', value);
+      this.invoke('save');
+      return value;
+    }
   }.property('@each.isCompleted'),
 
   inflection: function() {
